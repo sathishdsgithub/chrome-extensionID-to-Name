c973f00a143022fd23a21bc2c06d31549afa51e8
sathishdsgithub <sathish.damodaran.suresh@gmail.com>
2020-07-16 13:25:01 +0200

    Update chrome_extension_id_name.sh

--- chrome_extension_id_name.sh
+++ chrome_extension_id_name.sh
@@ -15,7 +15,9 @@ hipbfijinpcgfogaopmgehiegacbhmob
 
 #!/bin/bash
 
-for i in `cat /tmp/extension_id` ; do wget -O - https://chrome.google.com/webstore/detail/$i --max-redirect 0 --append-output=/tmp/zlogfile >> /tmp/zlogfile \
-; echo "$i Completed" ;    done
-
+for i in `cat /tmp/extension_id.txt` ; do wget -O - https://chrome.google.com/webstore/detail/$i --max-redirect 0 --append-output=/tmp/zlogfile >> /tmp/zlogfile ;\
+echo "$i Completed" ;  done
+echo -e "\n \t #### Corresponding Name of the Extension ID #### \t\n"
 cat /tmp/zlogfile |  grep following | awk -F / '{print $(NF-1),"\r\t",$NF}' | sed "s/\[following\]//"
+echo -e "\n \t #### Corresponding Name  #### \t\n"
+cat /tmp/zlogfile |  grep following | awk -F / '{ print $(NF-1) }'
